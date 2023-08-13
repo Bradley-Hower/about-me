76eb4403b9b7e226b666b3382de36478f89824d2
Bradley Hower <bradleyhower@proton.me>
2023-08-12 17:58:22 -0700

    removal of console logs

--- js/app.js
+++ js/app.js
@@ -12,9 +12,7 @@ function quiz(){
   let scorecount = 0;
   // Question1
   let response1_feedback = 'You didn\'t answer...';
-
   let response1 = prompt('Did I grow up in the Seattle area?').toLowerCase();
-  // console.log(response1);
 
   if (response1 === 'y' || response1 === 'yes'){
     response1_feedback = 'Wow! Nice job!';
@@ -24,14 +22,11 @@ function quiz(){
   } else {
     response1_feedback = 'Sorry. I do not understand.';
   }
-  // console.log(response1_feedback)
   alert(response1_feedback);
 
   // Question2
   let response2_feedback = 'You didn\'t answer...';
-
   let response2 = prompt('Did I go to university?').toLowerCase();
-  // console.log(response2);
 
   if (response2 === 'y' || response2 === 'yes'){
     response2_feedback = 'Wow! Nice job!';
@@ -41,14 +36,11 @@ function quiz(){
   } else {
     response2_feedback = 'Sorry. I do not understand.';
   }
-  // console.log(response2_feedback)
   alert(response2_feedback);
 
   // Question3
   let response3_feedback = 'You didn\'t answer...';
-
   let response3 = prompt('Did I work in a hospital?').toLowerCase();
-  // console.log(response3);
 
   if (response3 === 'y' || response3 === 'yes'){
     response3_feedback = 'Sorry. Incorrect.';
@@ -58,14 +50,11 @@ function quiz(){
   } else {
     response3_feedback = 'Sorry. I do not understand.';
   }
-  // console.log(response3_feedback)
   alert(response3_feedback);
 
   // Question4
   let response4_feedback = 'You didn\'t answer...';
-
   let response4 = prompt('Or did I work in mortages?').toLowerCase();
-  // console.log(response4);
 
   if (response4 === 'y' || response4 === 'yes'){
     response4_feedback = 'Wow! Nice job!';
@@ -75,14 +64,11 @@ function quiz(){
   } else {
     response4_feedback = 'Sorry. I do not understand.';
   }
-  // console.log(response1_feedback)
   alert(response4_feedback);
 
   // Question5
   let response5_feedback = 'You didn\'t answer...';
-
   let response5 = prompt('Do I have any children?').toLowerCase();
-  // console.log(response5);
 
   if (response5 === 'y' || response5 === 'yes'){
     response5_feedback = 'Wow! Nice job!';
@@ -92,7 +78,6 @@ function quiz(){
   } else {
     response5_feedback = 'Sorry. I do not understand.';
   }
-  // console.log(response5_feedback)
   alert(response5_feedback);
 
   // Question6
@@ -102,12 +87,10 @@ function quiz(){
   let response6 = 0;
   while (response6 !== 2 && attemptcountq6 < 4){
     let response6 = Number(prompt('Guess how many dogs I have.'));
-    // console.log(response6);
 
     if (response6 === 2){
       alert('You got it!');
       scorecount++;
-      // console.log(scorecount);
       break;
     } else if (response6 > 2){
       if (attemptcountq6 < 3){
@@ -133,15 +116,12 @@ function quiz(){
 
   // Question 7
   let attemptcountq7 = 0;
-  const favoritethings = [Math.floor(Math.random() * (50 - 1) + 1), Math.floor(Math.random() * (50 - 1) + 1), Math.floor(Math.random() * (50 - 1) + 1)];
-  console.log(favoritethings);
+  const numberlotto = [Math.floor(Math.random() * (50 - 1) + 1), Math.floor(Math.random() * (50 - 1) + 1), Math.floor(Math.random() * (50 - 1) + 1)];
   question7:
   while (attemptcountq7 < 6){
     let guess = Number(prompt(`Guess a number from 1 through 50. There are three random ones generated. ${attemptcountq7+1}/6`));
-    // console.log(guess);
 
-    for (const thing of favoritethings){
-      // console.log(thing);
+    for (const thing of numberlotto){
       if (guess === thing){
         alert('You got one!');
         scorecount++;
@@ -151,19 +131,10 @@ function quiz(){
     attemptcountq7++;
   }
 
-  alert(`The numbers were ${favoritethings[0]}, ${favoritethings[1]}, and ${favoritethings[2]}.`)
+  alert(`The numbers were ${numberlotto[0]}, ${numberlotto[1]}, and ${numberlotto[2]}.`);
 
   alert(`${username}, thank you for taking my quiz. You got ${scorecount}/7 questions correct. I hope you learned a little bit about me.`);
 }
 
 
 quiz();
-
-
-
-
-// Did I grow up in the Seattle area?
-// Did I go to university?
-// Did I work in a hospital?
-// Or did I work in mortages?
-// Do I have any children?
